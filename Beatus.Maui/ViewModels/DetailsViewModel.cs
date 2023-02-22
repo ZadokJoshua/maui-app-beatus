e270f51d3e0ec2338fe937d93550c844084bf9b9
Zadok Joshua <Ayegbazadok@outlook.com>
2023-02-22 22:43:04 +0100

    Add SavedViewModel

--- Beatus.Maui/ViewModels/DetailsViewModel.cs
+++ Beatus.Maui/ViewModels/DetailsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Beatus.Maui.Models;
 using Beatus.Maui.Services;
+using Beatus.Maui.Services.Interfaces;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -10,6 +11,7 @@ public partial class DetailsViewModel : ObservableObject
 {
     private PredictionDetails predictionDetails;
     private readonly OpenAiService _openAiService;
+    private readonly IDataService _dataService;
 
     public PredictionDetails PredictionDetails
     {
@@ -22,13 +24,28 @@ public partial class DetailsViewModel : ObservableObject
         }
     }
 
-    public DetailsViewModel(OpenAiService openAiService)
+    public DetailsViewModel(OpenAiService openAiService, IDataService dataService)
     {
         _openAiService = openAiService;
+        _dataService = dataService;
     }
 
     public ImageSource PlantImage => ImageSource.FromStream(() => new MemoryStream(PredictionDetails.PlantImage));
 
     [RelayCommand]
     public void PreviousPage() => Shell.Current.GoToAsync("..");
+
+    [RelayCommand]
+    public async Task SavePrediction()
+    {
+        PredictionDetailsEntity entity = new PredictionDetailsEntity()
+        {
+            PlantImage = PredictionDetails.PlantImage,
+            TagName = PredictionDetails.TagName,
+            Probability = PredictionDetails.Probability,
+            Recommendation = PredictionDetails.Recommendation
+        };
+
+        await _dataService.SavePrediction(entity);
+    }
 }
---
+++ Beatus.Maui/ViewModels/SavedViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Beatus.Maui.Models;
+using Beatus.Maui.Services.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace Beatus.Maui.ViewModels;
+
+public class SavedViewModel
+{
+    private readonly IDataService _dataService;
+    public ObservableCollection<PredictionDetailsEntity> SavedPredictions { get; set; } = new ObservableCollection<PredictionDetailsEntity>();
+
+    public SavedViewModel(IDataService dataService)
+    {
+        _dataService = dataService;
+        LoadSavedPredictions().Wait();
+    }
+
+    public async Task LoadSavedPredictions()
+    {
+        var predictions = await _dataService.GetAllSavedPredictionsAsync();
+        SavedPredictions.Clear();
+        foreach (var prediction in predictions)
+        {
+            SavedPredictions.Add(prediction);
+        }
+    }
+
+}
