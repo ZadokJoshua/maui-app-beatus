7783fcd190e61c2813fbce672483a8f6dbab8aef
Zadok Joshua <Ayegbazadok@outlook.com>
2023-02-25 20:31:56 +0100

    Fix bugs in SavedPage and SavedViewModel - enabled nullable in this class.

--- Beatus.Maui/ViewModels/SavedViewModel.cs
+++ Beatus.Maui/ViewModels/SavedViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Beatus.Maui.Models;
+﻿#nullable enable
+using Beatus.Maui.Models;
 using Beatus.Maui.Services.Interfaces;
 using Beatus.Maui.Views;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -12,14 +13,14 @@ public partial class SavedViewModel : ObservableObject
     private readonly IDataService _dataService;
 
     [ObservableProperty]
-    private PredictionDetailsEntity selectedPrediction;
+    private PredictionDetailsEntity? selectedPrediction;
     [ObservableProperty]
     private bool isBusy;
     [ObservableProperty]
     private bool isPredictionEmpty;
 
-
-    public ObservableCollection<PredictionDetailsEntity> SavedPredictions { get; private set; }
+    [ObservableProperty]
+    private ObservableCollection<PredictionDetailsEntity>? savedPredictions;
 
     public SavedViewModel(IDataService dataService)
     {
@@ -37,40 +38,44 @@ public partial class SavedViewModel : ObservableObject
             return;
         }
         
-        SavedPredictions.Clear();
+        SavedPredictions?.Clear();
+        
         foreach (var prediction in predictions)
         {
-            SavedPredictions.Add(prediction);
+            SavedPredictions?.Add(prediction);
         }
     }
 
     [RelayCommand]
-    public async Task ViewPredictionDetails()
+    async Task ViewPredictionDetails()
     {
         IsBusy = true;
+        
         if (SelectedPrediction != null)
         {
-            PredictionDetails prediction = new()
-            {
-                PlantImage = SelectedPrediction.PlantImage,
-                TagName = SelectedPrediction.TagName,
-                Probability = SelectedPrediction.Probability,
-                Recommendation = SelectedPrediction.Recommendation
-            };
+            PredictionDetails? prediction = new()
+        {
+            PlantImage = SelectedPrediction.PlantImage,
+            TagName = SelectedPrediction.TagName,
+            Probability = SelectedPrediction.Probability,
+            Recommendation = SelectedPrediction.Recommendation
+        };
 
+        
             await Shell.Current.GoToAsync(nameof(DetailsPage), new Dictionary<string, object>
+        {
             {
-                {
-                    "PredictionDetails", prediction
-                },
-                {
-                    "IsOpenedFromMainPage", false
-                },
-                {
-                    "EntityId", SelectedPrediction.Id
-                }
-            });
+                "PredictionDetails", prediction
+            },
+            {
+                "IsOpenedFromMainPage", false
+            },
+            {
+                "EntityId", SelectedPrediction.Id
+            }
+        }); 
         }
+
         IsBusy = false;
     }
 }
